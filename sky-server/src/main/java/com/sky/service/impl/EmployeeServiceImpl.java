385018ed4a3a6859f5dd7ee17aade451ed172c1a
Elvis-ss <1437613764@qq.com>
2026-01-06 21:54:58 +0800

    员工状态更改部分代码开发完毕

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -104,4 +104,21 @@ public class EmployeeServiceImpl implements EmployeeService {
         return new PageResult(total,records);
     }
 
+    /**
+     * 启用，禁用员工账号，本质是更新操作
+     * @param status
+     * @param id
+     * @return
+     */
+    @Override
+    public void startOrStop(Integer status, Long id) {
+        Employee employee = new Employee();
+        employee.setStatus(status);
+        employee.setId(id);
+        employeeMapper.update(employee);
+
+
+    }
+
+
 }
